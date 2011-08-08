abfd608c8d7f618ff108ea115c7c5ca5cd40bc4f
Shekhar <pythonic@gmail.com>
2011-08-09 00:59:37 +0530

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphc',
-    version='0.5.9',
+    version='0.7.1',
     description='Simple Pythonic HTML Creator',
     long_description=open("README.rst").read(),
     packages=find_packages(),
