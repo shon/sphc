851646fbe4076b07984b4faeaee5f4c122e3a015
Shekhar <pythonic@gmail.com>
2011-08-24 14:28:06 +0530

    tag ordering corrected. Navigation simplified and removed implementation

--- sphc/more.py
+++ sphc/more.py
@@ -13,22 +13,22 @@ css_links = [ 'https://github.com/thatcoolguy/gridless-boilerplate/raw/master/as
 class HTML5Page(object):
     """
     Common case HTML5 template
-    Mostly based on HTML5 Boilerplate
+    Loosely based on HTML5 Boilerplate
 
-    nav: list eg. [('Home', '/', None), ('About Us', '/about', None)]
-        Also supports higly flexible navigation options such as
+    nav_menu: list eg. [('Home', '/', None), ('About Us', '/about', None)]
+        Also supports higly flexible navigation options as shown in example below
         >>> home_opts = [tf.A('Home Option 1', href='/home/opt_1'), tf.A('Home Option 2', href='/home/opt_2')]
         >>> profile_opts = [tf.A('Profile Option 1', href='/profile/opt_1'), tf.A('Profile Option 2', href='/profile/opt_2')]
-        >>> nav_links = [
+        >>> nav_menu = [
             ('Home', '#home', home_opts),
-            ('Invoicing', '#home', profile_opts)
-            ]
+            ('Invoicing', '#home', profile_opts) ]
     """
     doctype = "<!DOCTYPE html>"
     jslibs = gen_jquery_urls()
     css_links = css_links
     title = "Common case HTML5 template"
-    nav_links = []
+    nav_menu = []
+    current_nav = ""
 
     def head(self):
         head = tf.HEAD()
@@ -53,48 +53,19 @@ class HTML5Page(object):
         return ''
 
     def nav(self):
-        if not self.nav_links: return ''
+        if not self.nav_menu: return ''
         nav = tf.NAV()
-        nav.links_container = tf.UL()
-        link_opts = tf.DIV(Class='navlink-opts')
-        c = itertools.count()
-        for label, url, opt in self.nav_links:
-            link_id = c.next()
-            li = tf.LI()
-            current = label == self.current_tab
-            Class = "navlink navlink-current" if current else "navlink"
-            li.link = tf.A(label, Class=Class, id=('navlink-%s' % link_id), href=url)
-            nav.links_container.li = li
+        menu = []
+        for label, url, opt in self.nav_menu:
+            header = tf.H2()
+            header.link = tf.A(label, href=url)
+            opt_box = tf.DIV(Class="nav-opt")
             if opt:
-                opt_container = tf.DIV(Class="navlink-opt", id=('navlink_opt-%s' % link_id))
-                opt_container.opt = opt
-                link_opts.opt_container = opt_container
-        #if hasattr(nav.links_container, 'li'):
-        #    nav.links_container.li[0].link.attributes['Class'] = 'navlink navlink-current'
-        nav.clear = clear()
-        nav.link_opts = link_opts
-        script = sphc.tf.SCRIPT("""
-        $('.navlink-opt').hide();
-        $('.navlink').click( function () {
-            $('.navlink-current').attr('class', 'navlink');
-            $(this).attr('class', 'navlink-current');
-            var navlink_opt_id = 'navlink_opt-' + $(this).attr('id').split('-')[1];
-            $('.navlink-opt').hide();
-            $('#' + navlink_opt_id).show();
-        });
-        $('.navlink-current').click();
-        var curpath = window.location.pathname + window.location.hash + window.location.search;
-        $('.navlink-opt-item').each( function () {
-            if ($(this).attr('href') == curpath) {
-                $(this).attr('class', 'navlink-opt-item navlink-opt-item-current');
-            };
-        });
-        /*
-        $('.navlink-opt-item').click( function () {
-            $('.navlink-opt-item-current').attr('class', 'navlink-opt-item');
-        }); */
-        """)
-        nav.script = script
+                opt_box.opt = opt
+                if label == self.current_nav:
+                    opt_box.add_classes(["nav-opt-current"])
+            menu.extend([header, opt_box])
+        nav.menu = menu
         return nav
 
     def render(self):
@@ -103,13 +74,12 @@ class HTML5Page(object):
         html.body = tf.BODY()
         html.body.topbar = self.topbar()
         html.body.clear = clear()
-        html.body.nav = self.nav()
-        html.body.clear = clear()
         html.body.container = tf.DIV(id="container")
+        html.body.container.nav = self.nav()
         html.body.container.header = self.header()
         html.body.container.main = tf.DIV(id="main", role="main")
         html.body.container.main.main = self.main()
-        html.body.footer = self.footer()
+        html.body.container.footer = self.footer()
         html.body.clear = clear()
         html.body.bottombar = self.bottombar()
         return self.doctype + str(html)
