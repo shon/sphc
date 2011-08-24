2b95b337aa0b365055394e17311d2d02f27cea08
Gaurav Rai <gaurav.hub@gmail.com>
2011-08-24 18:27:19 +0530

    Addition of Anchor tag in unfriendly self closing tag

--- sphc/__init__.py
+++ sphc/__init__.py
@@ -1,7 +1,7 @@
 import cgi
 
 ESCAPE_DEFAULT = True
-TAGS_UNFRIENDLY_WITH_SELF_CLOSING = ('DIV', 'SELECT', 'TEXTAREA', 'SCRIPT')
+TAGS_UNFRIENDLY_WITH_SELF_CLOSING = ('DIV', 'SELECT', 'TEXTAREA', 'SCRIPT', 'A')
 
 class pats:
     regular = '<%(tagname)s%(nv_attributes)s%(attributes)s>%(content)s%(children)s</%(tagname)s>'
